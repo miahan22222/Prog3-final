321801952b2b9891efd03fcb16608c2f0c68b00e
mi usuario de github <mimihanono@gmail.com>
2024-11-11 11:48:50 -0300

    ¨hola¨

---
+++ Prog3-final/src/firebase/config.js
@@ -0,0 +1,17 @@
+import app from "firebase/app"
+import firebase from "firebase/app";
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDu9mhTfQQdK5EkdsTUSSuqF2IsR2ITLaU",
+    authDomain: "hola-2aea5.firebaseapp.com",
+    projectId: "hola-2aea5",
+    storageBucket: "hola-2aea5.firebasestorage.app",
+    messagingSenderId: "12597978674",
+    appId: "1:12597978674:web:c225aadc9993a4a2853e2b"
+  };
+  app.initializeApp(firebaseConfig)
+
+export const auth = firebase.auth()
+export const db = app.database()
+export const storage = app.storage()
